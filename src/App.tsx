044ccc6fcc0cc2a448febd0f945d7f52dbfc3f19
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-03 22:18:10 +0100

    added a dimmer switch for the sake of my eyes. Will fix the styling later
    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PokeDetails, PokeList } from './components';
-import { useGetAllPokemon } from './api/api';
+import { getStorageIsDimmed, setStorageIsDimmed } from './@/lib/utils';
+import { Switch } from './@/components/ui/switch';
 
 const App = () => {
+  const [isDimmed, setIsDimmed] = useState(getStorageIsDimmed());
+
   return (
-    <div className="h-screen p-4 bg-zinc-300 flex justify-center items-center">
-      <div className="h-[70%] w-[70%] p-10 rounded-l-lg border-2 border-black bg-red-400">
-        <div className="h-full w-full grid grid-cols-[1fr,1rem,1fr]">
-          <PokeList />
-          <div className="w-5"></div>
-          <PokeDetails />
+    <div>
+      <Switch
+        className=""
+        checked={isDimmed}
+        onCheckedChange={() => {
+          setStorageIsDimmed(!isDimmed);
+          setIsDimmed(!isDimmed);
+        }}
+      />
+      <div
+        className={`h-screen p-4 ${isDimmed ? 'bg-slate-800' : 'bg-zinc-300'} flex justify-center items-center`}
+      >
+        <div className="h-[70%] w-[70%] p-10 rounded-l-lg border-2 border-black bg-red-400">
+          <div className="h-full w-full grid grid-cols-[1fr,1rem,1fr]">
+            <PokeList />
+            <div className="w-5"></div>
+            <PokeDetails />
+          </div>
         </div>
       </div>
     </div>

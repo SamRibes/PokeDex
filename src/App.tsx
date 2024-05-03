3598ad726842e2d2e503862e535eaf9b601073e4
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-03 21:18:25 +0100

    basic layout and some styling for pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useGetAllPokemon } from './api/api';
-import PokeCard from './components/PokeCard';
+import { PokeDex } from './components';
 
 const App = () => {
-  const { data, isLoading } = useGetAllPokemon();
-
   return (
-    <div className="h-screen p-4 bg-zinc-300">
-      {isLoading && <>...is loading...</>}
-      <div className="flex justify-left flex-wrap ">
-        {!isLoading &&
-          data?.results.map((pokemon, index) => (
-            <PokeCard key={index} pokemon={pokemon} index={index} />
-          ))}
-      </div>
+    <div className="h-screen p-4 bg-zinc-300 flex justify-center items-center">
+      <PokeDex />
     </div>
   );
 };
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as PokeDex } from './PokeDex';
+export { default as PokeCard } from './PokeCard';

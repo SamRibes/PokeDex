ff0ecc68a1ad22207a3b022f1af268921fd21909
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-03 21:24:26 +0100

    all pokemon in left side of pekedex with scrolling and styling

--- src/components/PokeDex.tsx
+++ src/components/PokeDex.tsx
@@ -8,14 +8,13 @@ const PokeDex = () => {
   return (
     <div className="h-[70%] w-[70%] p-10 rounded-l-lg border-2 border-black bg-red-400">
       <div className="h-full w-full grid grid-cols-[1fr,1rem,1fr]">
-        <div className="p-5 border-2 border-black bg-black">
-          <div className="p-5 flex justify-left flex-wrap bg-slate-500 h-full">
-            something
-            {/* {isLoading && <>...is loading...</>}
-        {!isLoading &&
-          data?.results.map((pokemon, index) => (
-            <PokeCard key={index} pokemon={pokemon} index={index} />
-          ))} */}
+        <div className="p-5 border-2 border-black bg-black overflow-auto">
+          <div className="p-5 flex justify-left flex-wrap bg-slate-500 h-full overflow-auto">
+            {isLoading && <>...is loading...</>}
+            {!isLoading &&
+              data?.results.map((pokemon, index) => (
+                <PokeCard key={index} pokemon={pokemon} index={index} />
+              ))}
           </div>
         </div>
         <div className="w-5"></div>

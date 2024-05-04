86567c2906dd6c28c06889d5ec0548d90757c7d2
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-04 13:45:16 +0100

    adjusted size of main section

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
       <div
         className={`h-screen p-4 ${isDimmed ? 'bg-slate-800' : 'bg-zinc-300'} flex justify-center items-center`}
       >
-        <div className="h-[70%] w-[70%] p-10 rounded-l-lg border-2 border-black bg-red-400">
+        <div className="h-[90%] w-[85%] p-10 rounded-l-lg border-2 border-black bg-red-400">
           <div className="h-full w-full grid grid-cols-[1fr,1rem,1fr]">
             <PokeList />
             <div className="w-5"></div>

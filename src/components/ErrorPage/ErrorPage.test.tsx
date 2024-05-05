056dd5cc531734bf6ff0385682cc9c42e3b8c467
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-05 19:25:28 +0100

    removed comment

--- src/components/ErrorPage/ErrorPage.test.tsx
+++ src/components/ErrorPage/ErrorPage.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ErrorPage from './ErrorPage';
 
-// we'll put some stuff in here later
-
 test('Renders the Error Page', () => {
   render(<ErrorPage />);
   const linkElement = screen.getByText(/Error Page/i);

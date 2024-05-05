0c7719c5d5f25da8fe7f6b8f4f20f72805345da7
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-05 20:53:43 +0100

    poke list tests

--- src/components/PokeList/PokeList.test.tsx
+++ src/components/PokeList/PokeList.test.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import nock from 'nock';
+import PokeList from './PokeList';
+import { interceptAPICalls, AllTheProviders } from '../../test-utils';
 
-test('Renders the Error Page', () => {
-  // render(<PokeCard pokemon={} index={} />);
-  // const linkElement = screen.getByText(/Error Page/i);
-  // expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+  interceptAPICalls();
+});
+
+afterAll(() => {
+  nock.cleanAll();
+});
+
+test('Renders the PokeList Component correctly', () => {
+  render(
+    <AllTheProviders initialEntries="/">
+      <PokeList />
+    </AllTheProviders>,
+  );
+  const pokemonName1 = screen.getByText(/Bulbasaur/i);
+  expect(pokemonName1).toBeInTheDocument();
+  const pokemonName2 = screen.getByText(/Ivysaur/i);
+  expect(pokemonName2).toBeInTheDocument();
+  const pokemonName3 = screen.getByText(/Venusaur/i);
+  expect(pokemonName3).toBeInTheDocument();
+});
+
+test('Search works', () => {
+  const user = userEvent.setup();
+
+  render(
+    <AllTheProviders initialEntries="/">
+      <PokeList />
+    </AllTheProviders>,
+  );
+
+  const searchInput = screen.getByPlaceholderText('Search');
+  expect(searchInput).toBeInTheDocument();
+  user.type(searchInput, 'bulbasaur');
+
+  const pokemonName1 = screen.getByText(/Bulbasaur/i);
+  expect(pokemonName1).toBeInTheDocument();
+  const pokemonName2 = screen.getByText(/Ivysaur/i);
+  expect(pokemonName2).toNotBeInTheDocument();
+  const pokemonName3 = screen.getByText(/Venusaur/i);
+  expect(pokemonName3).toNotBeInTheDocument();
 });

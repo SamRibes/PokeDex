68d946a13c4dc2546c3e1443d6bea5407e4a8bdb
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-05 20:54:06 +0100

    don't need these

--- src/components/Spinner/Spinner.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-
-test('Renders the Error Page', () => {
-  // render(<PokeCard pokemon={} index={} />);
-  // const linkElement = screen.getByText(/Error Page/i);
-  // expect(linkElement).toBeInTheDocument();
-});

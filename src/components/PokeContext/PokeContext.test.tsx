683313ec30f7f61dfdab0f451e82559388c1c13c
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-05 18:10:00 +0100

    renamed .test.tsx files to proper names

---
+++ src/components/PokeContext/PokeContext.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { PokeContext } from './PokeContext';
+
+// The context provider here really isn't doing all that much.
+// We will test that it's working in the user flow tests
+
+test('Renders the PokeContext Component', () => {});

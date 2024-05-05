1424b6f88d4d05221a01e00a6f17127ca28e4d3a
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-05 23:00:25 +0100

    Just one test for user flow. Can't et the endpoint interception working so leaving it there

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import nock from 'nock';
+import { AllTheProviders, interceptAPICalls } from './test-utils';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
-export const TestingProvider = (testComponent: JSX.Element) => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: testComponent,
-    },
-  ]);
+// TODO- Having trouble getting the interceptor to catch urls with query params.
 
-  const queryClient = new QueryClient();
+beforeAll(() => interceptAPICalls());
+afterAll(() => nock.cleanAll());
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+test('Click on Bulbasaur and correct description is displayed', async () => {
+  const user = userEvent.setup();
+  render(
+    <AllTheProviders initialEntries="/">
+      <App />
+    </AllTheProviders>,
   );
-};
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const bulbasaur = await screen.findByText('Bulbasaur', undefined, {
+    timeout: 500,
+  });
+  user.click(bulbasaur);
+
+  const description = await screen.findByText(
+    'A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.',
+    undefined,
+    {
+      timeout: 500,
+    },
+  );
+  expect(description).toBeInTheDocument();
 });

72a1e042acf98cd12e6204464d3b4d628d60313f
Samuel Ribeiro <samuel.c.ribeiro@outlook.com>
2024-05-05 20:30:14 +0100

    PokeDetails test

--- src/components/PokeDetails/PokeDetails.test.tsx
+++ src/components/PokeDetails/PokeDetails.test.tsx
@@ -4,13 +4,49 @@ import { AllTheProviders } from '../../test-utils';
 import PokeDetails from './PokeDetails';
 
 test('Renders the PokeDetails Component correctly', async () => {
-  render(
+  const { container } = render(
     <AllTheProviders initialEntries="?pokeId=1">
       <PokeDetails />
     </AllTheProviders>,
   );
 
   await new Promise((r) => setTimeout(r, 500));
-  const linkElement = await screen.getByText(/Bulbasaur/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const pokemonName = screen.getByText(/Bulbasaur/i);
+  expect(pokemonName).toBeInTheDocument();
+
+  const description = screen.getByText(
+    /A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON./i,
+  );
+  expect(description).toBeInTheDocument();
+
+  const hp = await screen.getByText(/Hp/i);
+  expect(hp).toBeInTheDocument();
+  const hpValue = await screen.getByText(/45/i);
+  expect(hpValue).toBeInTheDocument();
+
+  const attack = await screen.getByText(/Attack/i);
+  expect(attack).toBeInTheDocument();
+  const attackValue = await screen.getByText(/49/i);
+  expect(attackValue).toBeInTheDocument();
+
+  const defense = await screen.getByText(/Defense/i);
+  expect(defense).toBeInTheDocument();
+  const defenseValue = await screen.getByText(/49/i);
+  expect(defenseValue).toBeInTheDocument();
+
+  const specialAttack = await screen.getByText(/Special-attack/i);
+  expect(specialAttack).toBeInTheDocument();
+  const specialAttackValue = await screen.getByText(/65/i);
+  expect(specialAttackValue).toBeInTheDocument();
+
+  const specialDefense = await screen.getByText(/Special-defense/i);
+  expect(specialDefense).toBeInTheDocument();
+  const specialDefenseValue = await screen.getByText(/65/i);
+  expect(specialDefenseValue).toBeInTheDocument();
+
+  const speed = await screen.getByText(/Speed/i);
+  expect(speed).toBeInTheDocument();
+  const speedValue = await screen.getByText(/45/i);
+  expect(speedValue).toBeInTheDocument();
 });
